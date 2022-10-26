018827a25bc277e55eafeedc21de5b1b0f7ec7cf
skana1 <estera.skana01@gmail.com>
2022-10-26 15:54:14 +0200

    First commit

---
+++ src/main/java/com/example/restservice/MainController.java
@@ -0,0 +1,22 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MainController {
+
+    private static final String template ="SpringAPI, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/main")
+    @ResponseBody
+    public Main sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Main(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
